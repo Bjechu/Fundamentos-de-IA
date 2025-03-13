8292a711871ac3f6b3f0721741a872ef8d7ea21a
Bjechu <112441034+Bjechu@users.noreply.github.com>
2025-03-12 20:45:51 -0600

    A01.py
    Minímos Cuadrados

--- A01.py
+++ A01.py
@@ -1,4 +1,6 @@
+#==========================
 # Modulos necesarios
+#==========================
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,12 +11,28 @@ def minimos_cuadrados(X,Y):
     N = len(X)
     sumx = np.sum(X)
     sumy = np.sum(Y)
-    sumXY = np.sum(X*Y)
+    sumxy = np.sum(X*Y)
     sumx2 = np.sum(X*X)
     w1 = (N*sumxy - sumx*sumy)/(N*sumx2 - sumx*sumx)
     w0 = (sumy - w1*sumx)/N
     Ybar = w0 + w1*X
+    return Ybar,w0,w1
 #================================
 # Programa Principal 
 #================================
-if __name__ == ¨__main__¨
+if __name__ == "__main__"
+    #===============
+    # Leer Datos
+    #===============
+    data = pd.read_csv('data.csv')
+    X = data.iloc[:, 0]
+    Y = data.iloc[:, 1]
+    Ybar,w0,w1 = minimos_cuadrados(X, Y)
+    #===============
+    # Gráfica
+    #===============
+    plt.scatter(X, Y)
+    plt.rcParams['figure.figsize'] = (12.0, 9.0)
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.show()
